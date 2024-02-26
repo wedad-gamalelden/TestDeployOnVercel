dce83d46f2e38b59c61f530bff9df6a9ce697fae
wedad-gamalelden <wedadgamalelden@gmail.com>
2024-02-26 16:35:29 +0200

    Inital commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.          
         </p>
+        <p>Deployed by Wedad for test deploy on vercel</p>
         <a
           className="App-link"
           href="https://reactjs.org"
